4d5b02190f80af6374466b6a2ff195f791718dbd
ressstart <lressstart@gmail.com>
2018-03-16 19:59:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/pc/jumperv1/GameView.java
@@ -0,0 +1,92 @@
+package com.example.pc.jumperv1;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.CountDownTimer;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by PC on 25.01.2018.
+ */
+
+public class GameView extends View{
+    private int viewWidth; // ширина и высота игрового поля
+    private int viewHeight;
+    private int points = 0;
+    private Sprite playerBird;
+    private final int timerInterval = 30;
+
+    public GameView(Context context) {
+        super(context);
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.sprites2var);
+        int w = b.getWidth()/7;
+        int h = b.getHeight()/1;
+        Rect firstFrame = new Rect(0,0, w, h);
+        playerBird = new Sprite(10,0,0,0, firstFrame, b);
+
+        class Timer extends CountDownTimer {
+            public Timer() {
+                super(Integer.MAX_VALUE, timerInterval);
+            }
+            @Override
+            public void onTick(long millisUntilFinished) {
+                update ();
+            }
+            @Override
+            public void onFinish() {
+            }
+        }
+
+        Timer t = new Timer();
+        t.start();
+        for (int i = 0; i < 7; i++) {
+                playerBird.addFrame(new Rect(i * w, 0, i * w + w, h));
+        }
+    }
+
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+
+        viewWidth = w;
+        viewHeight = h;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int eventAction = event.getAction();
+
+        if (eventAction == MotionEvent.ACTION_DOWN)  {
+            playerBird.jump();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){ // чтобы рисовать на поверхности компонента
+        super.onDraw(canvas);
+        //canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.sprites2var),20,15,null); //32-37 фон и кол-во очков на поверх-и компонента
+        canvas.drawARGB(100,200,220,100);
+        Paint p = new Paint();
+        p.setAntiAlias(true);
+        p.setTextSize(55.0f);
+        p.setColor(Color.BLUE);
+        canvas.drawText(points+"", viewWidth - 100, 70, p);
+        playerBird.draw(canvas);
+    }
+
+    protected void update () {
+        playerBird.update(timerInterval);
+        invalidate();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pc/jumperv1/Sprite.java
@@ -0,0 +1,157 @@
+package com.example.pc.jumperv1;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.Image;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by PC on 25.01.2018.
+ */
+
+public class Sprite {
+    ImageView image;
+    private Bitmap bitmap; //ссылка на изображение с набором кадров
+    private List<Rect> frames; //коллекшн прямоугол областей на изобр - кадры,участвующ в анимац послед-ти
+    private int frameWidth; // ширина объекта
+    private int frameHeight; // высота объекта
+    private int currentFrame; // номер текущего кадра в коллекции frames
+    private double frameTime; // время отведенное на отображ каждого кадра анимац послед-ти
+    private double timeForCurrentFrame; // текущее время отображ кадра.
+                                        // номер текущ кадра currentFrame меняется на след при достиж-и
+                                        //перемен timeForCurrentFrame значения из frameTime
+    private double x; // местоположение левого верхнего угла спрайта на экране
+    private double y;
+    private double velocityX; //скорости перемещения по осям Ох и Оу
+    private double velocityY;
+    private int padding; // внутренний отступ от границ спрайта для более точн опр пересеч спрайтов
+    private boolean isJumping = false;
+
+    //ImageView imageView = (image).findViewById(R.id.);
+
+    public Sprite(double x, double y, double velocityX, double velocityY, Rect initialFrame, Bitmap bitmap){
+        this.x = x;
+        this.y = y;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.bitmap = bitmap;
+        this.frames = new ArrayList<Rect>();
+        this.frames.add(initialFrame);
+        this.bitmap = bitmap;
+        this.timeForCurrentFrame = 0.0;
+        this.frameTime = 45;
+        this.currentFrame = 0;
+        this.frameWidth = initialFrame.width();
+        this.frameHeight = initialFrame.height();
+        this.padding = 20;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public int getFrameWidth() {
+        return frameWidth;
+    }
+    public void setFrameWidth(int frameWidth) {
+        this.frameWidth = frameWidth;
+    }
+    public int getFrameHeight() {
+        return frameHeight;
+    }
+    public void setFrameHeight(int frameHeight) {
+        this.frameHeight = frameHeight;
+    }
+    public double getVx() {
+        return velocityX;
+    }
+    public void setVx(double velocityX) {
+        this.velocityX = velocityX;
+    }
+    public double getVy() {
+        return velocityY;
+    }
+    public void setVy(double velocityY) {
+        this.velocityY = velocityY;
+    }
+    public int getCurrentFrame() {
+        return currentFrame;
+    }
+    public void setCurrentFrame(int currentFrame) {
+        this.currentFrame = currentFrame%frames.size();
+    }
+    public double getFrameTime() {
+        return frameTime;
+    }
+    public void setFrameTime(double frameTime) {
+        this.frameTime = Math.abs(frameTime);
+    }
+    public double getTimeForCurrentFrame() {
+        return timeForCurrentFrame;
+    }
+    public void setTimeForCurrentFrame(double timeForCurrentFrame) {
+        this.timeForCurrentFrame = Math.abs(timeForCurrentFrame);
+    }
+    public int getFramesCount() {
+        return frames.size();
+    }
+    public void addFrames(Rect frame){ //метод добавления кадров в анимац послед-ть
+        frames.add(frame);
+    }
+
+    public void addFrame (Rect frame) {
+        frames.add(frame);
+    }
+
+    public void update(int ms){ // обновление внутреннего состояния спрайта, анимация.
+                                // вызов таймера с опр переодич-ю. в метод передается время в мс (с момента
+                                // последнего вызова этого метода). Время испол-ся для изм-я сост спрайта
+                                // его перемещ в простр-е за это время и расчит необход-ь перехода к след кадру
+        if (isJumping) {
+            timeForCurrentFrame += ms;
+
+            if (timeForCurrentFrame >= frameTime) {
+                if (currentFrame + 1 == frames.size())
+                    isJumping = false;
+                else
+                    currentFrame = currentFrame + 1;
+                timeForCurrentFrame = timeForCurrentFrame - frameTime;
+            }
+        }
+        x = x + velocityX * ms/1000.0;
+        y = y + velocityY * ms/1000.0;
+    }
+
+    public void jump() {
+        isJumping = true;
+        currentFrame = 0;
+    }
+
+    public void draw(Canvas canvas){ //этот метод рисует на переданном холсте текущ кадр frames.get(currentFrame)
+                                     //в области экрана, заданной в прямоугольнике destination
+        Paint p = new Paint();
+        Rect destination = new Rect((int)x, (int)y, (int)(x + frameWidth), (int)(y + frameHeight));
+        canvas.drawBitmap(bitmap, frames.get(currentFrame), destination, p);
+    }
+    public Rect getBoundBoxRect(){ // метод, возвращ прямоугол обл,участ-ую в опред-и столкновений
+        return new Rect((int)x+padding, (int)y+padding,(int)(x+frameWidth - 2*padding),
+                (int)(y + frameHeight - 2*padding));
+    }
+
+    public boolean intersect(Sprite s){ // метод опред-я получения спрайтов
+        return getBoundBoxRect().intersect(s.getBoundBoxRect());
+    }
+}
